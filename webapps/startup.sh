cb345d7e2e81339ace4fae124c2b1cc109f4dbe1
youngmin1205 <youngshinmin1205@gmail.com>
2024-07-30 00:00:14 +0900

    change

---
+++ webapps/startup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+BUILD_WAR=$(ls /home/tomcat/apache-tomcat-9.0.91/webapps/ROOT/*.war)
+WAR_NAME=$(basename $BUILD_WAR)
+echo "## build 파일명: $WAR_NAME"
+ 
+echo "## build 파일 복사"
+DEPLOY_PATH=/home/tomcat/apache-tomcat-9.0.91/webapps/ROOT/
+cp $BUILD_WAR $DEPLOY_PATH
+
+CURRENT_PID=$(pgrep -f $WAR_NAME)
+echo "## 현재 실행중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z $CURRENT_PID ]
+then
+  echo "## 현재 구동중인 애플리케이션 없음"
+else
+  echo "## kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+DEPLOY_WAR=$DEPLOY_PATH$WAR_NAME
+echo "## DEPLOY_WAR: $DEPLOY_WAR"
+nohup java -jar $DEPLOY_WAR  > $DEPLOY_PATH/nohup.out 2>&1 &
