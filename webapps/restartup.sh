b96553945a4017530a53c67ea1ae6d16cf208d41
youngmin1205 <youngshinmin1205@gmail.com>
2024-07-30 13:11:51 +0900

    change

--- webapps/restartup.sh
+++ webapps/restartup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 APP_DIR="/home/ec2-user/app"
-TOMCAT_WEBAPPS_DIR="/home/tomcat/apache-tomcat-9.0.91/webapps"
+TOMCAT_APPS_DIR="/home/tomcat/apachetomcat-9.0.91/webapps"
 
 echo "## Copying files"
-cp -r $APP_DIR/ROOT $TOMCAT_WEBAPPS_DIR/
+cp -r $APP_DIR/ROOT $TOMCAT_APPS_DIR/
 
 echo "## Stopping Tomcat server"
 sudo systemctl stop tomcat
