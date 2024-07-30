4094d65e06c4e57c79b3e34fe6bca6da825905ca
youngmin1205 <youngshinmin1205@gmail.com>
2024-07-30 11:32:04 +0900

    change

--- webapps/restartup.sh
+++ webapps/restartup.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-BUILD_WAR=$(ls /home/ec2-user/app/*.war)
-WAR_NAME=$(basename $BUILD_WAR)
-echo "## build 파일명: $WAR_NAME"
- 
-echo "## build 파일 복사"
-DEPLOY_PATH=/home/ec2-user/
-cp $BUILD_WAR $DEPLOY_PATH
 
-CURRENT_PID=$(pgrep -f $WAR_NAME)
-echo "## 현재 실행중인 애플리케이션 pid: $CURRENT_PID"
+APP_DIR="/home/ec2-user/app"
+TOMCAT_WEBAPPS_DIR="/path/to/tomcat/webapps"
 
-if [ -z $CURRENT_PID ]
-then
-  echo "## 현재 구동중인 애플리케이션 없음"
-else
-  echo "## kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
+echo "## Copying application files to Tomcat webapps directory"
+cp -r $APP_DIR/ROOT $TOMCAT_WEBAPPS_DIR/
 
-DEPLOY_WAR=$DEPLOY_PATH$WAR_NAME
-echo "## DEPLOY_WAR: $DEPLOY_WAR"
-nohup java -jar $DEPLOY_WAR  > $DEPLOY_PATH/nohup.out 2>&1 &
+echo "## Stopping Tomcat server"
+sudo systemctl stop tomcat
+
+sleep 5
+
+echo "## Starting Tomcat server"
+sudo systemctl start tomcat
+
+echo "## Deployment complete"
