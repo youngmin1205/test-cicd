77906cb0c1dc669fa1a52ba8a9ff0ee5f178be60
youngmin1205 <youngshinmin1205@gmail.com>
2024-08-01 18:02:09 +0900

    change

--- webapps/restartup.sh
+++ webapps/restartup.sh
@@ -5,7 +5,7 @@ TOMCAT_APPS_DIR="/home/tomcat/apachetomcat-9.0.91/webapps"
 TOMCAT_BIN_DIR="/home/tomcat/apache-tomcat-9.0.91/bin"
 
 echo "## Copying files"
-cp -rf $APP_DIR/ROOT $TOMCAT_APPS_DIR/
+\cp -rf $APP_DIR/ROOT $TOMCAT_APPS_DIR/
 
 echo "## Stopping Tomcat server"
 sudo $TOMCAT_BIN_DIR/shutdown.sh
