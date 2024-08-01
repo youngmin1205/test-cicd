a70db83cc64a445cf8ea72e47ac09c01bb4c3a37
youngmin1205 <youngshinmin1205@gmail.com>
2024-08-01 16:08:44 +0900

    change

--- webapps/restartup.sh
+++ webapps/restartup.sh
@@ -2,18 +2,17 @@
 
 APP_DIR="/home/ec2-user/app"
 TOMCAT_APPS_DIR="/home/tomcat/apachetomcat-9.0.91/webapps"
+TOMCAT_BIN_DIR="/home/tomcat/apache-tomcat-9.0.91/bin"
 
 echo "## Copying files"
 cp -r $APP_DIR/ROOT $TOMCAT_APPS_DIR/
 
 echo "## Stopping Tomcat server"
-
-cd /home/tomcat/apache-tomcat-9.0.91/bin
-./shutdown.sh
+sudo $TOMCAT_BIN_DIR/shutdown.sh
 
 sleep 5
 
 echo "## Starting Tomcat server"
-./startup.sh
+sudo $TOMCAT_BIN_DIR/shutdown.sh
 
 echo "## Deployment complete"
